729b519743626a73028a9362a4612263a312eac2
guptha- <guptha@cs.umass.edu>
2012-11-21 01:10:00 -0500

    Initial checkin: Added skeleton, md5 calculation, makefile

--- inc/prot.h
+++ inc/prot.h
@@ -10,7 +10,7 @@ void md5_append(md5_state_t *pms, const md5_byte_t *data, int nbytes);
 /* Finish the message and return the digest. */
 void md5_finish(md5_state_t *pms, md5_byte_t digest[16]);
 
-int md5CalcFileMD5 (char *fileName, char **hexDigest);
+int md5CalcFileMD5 (char *fileName, char *hexDigest);
 
-int md5CompareMD5 (char *MD5One, char *MD5Two)
+int md5CompareMD5 (char *MD5One, char *MD5Two);
 #endif /* prot_INCL */
--- src/md5manip.c
+++ src/md5manip.c
@@ -1,72 +1,47 @@
 #include "../inc/incl.h"
 
-/* Function name : md5FileToText
- * Description : Takes a file path as input and returns the contents of the file
- *               as a string.
- *               !! Free contents after using it!!
- */
-
-int md5FileToText (char *fileName, char **contents, long *fileSize)
+int md5FileToString (char *fileName, char **content, long *fileSize)
 {
-	long result;
-	FILE* fp = NULL;
-
-	fp = fopen(fileName, "r");
+	long result = 0;
+	FILE* fp = fopen(fileName, "r");
 	fseek(fp, 0, SEEK_END);
 	*fileSize = ftell(fp);
 	fseek(fp, 0, SEEK_SET);
-	*contents = malloc (sizeof(char) * *fileSize);
-	result = fread(*contents, 1, *fileSize, fp);
-
-	if (result == 0)
+	*content = malloc(sizeof(char) * *fileSize);
+	result = fread(*content, 1, *fileSize, fp);
+	if (result <= 0)
 	{
 		return FAILURE;
 	}
 	return SUCCESS;
 }
 
-/* Function name : md5CalcFileMD5
- * Description : Takes a file path as input and returns the MD5 hash of its
- * contents.
- */
 
 int md5CalcFileMD5 (char *fileName, char *hexDigest)
 {
-	md5_state_t state;
+  md5_state_t state;
 	md5_byte_t digest[16];
+	char *content = NULL;
 	long fileSize = 0;
 	int i = 0;
-	char *contents = NULL;
 
-	if (md5FileToText (fileName, &contents, &fileSize) == FAILURE)
+	if (md5FileToString (fileName, &content, &fileSize) == FAILURE)
 	{
 		return FAILURE;
 	}
 
-	/* Calling APIs to calculate the MD5 */
-	md5_init (&state);
-	md5_append (&state, (const md5_byte_t *) contents, fileSize);
-	md5_finish (&state, digest);
-
-	free (contents);
+	md5_init(&state);
+	md5_append(&state, content, fileSize);
+	md5_finish(&state, digest);
 
 	for (i = 0; i < 16; ++i)
 	{
-	  sprintf(hexDigest + i * 2, "%02x", digest[i]);
+		sprintf(hexDigest + i * 2, "%02x", digest[i]);
 	}
 
 	return SUCCESS;
 }
 
-/* Function name : md5CompareMD5
- * Description : Compares 2 given MD5 hashes and returns TRUE if they match
- */
 
-int md5CompareMD5 (char *MD5One, char *MD5Two)
-{
-  if (strncmp (MD5One, MD5Two, 16*2) == 0)
-	{
-		return TRUE;
-	}
-	return FALSE;
-}
+int md5CompareMD5 (char *MD5One, char *MD5Two);
+
